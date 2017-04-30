09696a39d6d3d2e4dba6600d6dbb4ba19e9f6c0f
keaton22 <keaton22@gmail.com>
2017-04-29 20:14:48 -0400

    implement feedback system

--- scripts/actions.js
+++ scripts/actions.js
@@ -27,19 +27,35 @@ function submitActionsForm(action, e) {
                     break;
                 case "setColorGreen":
                     setColor('green');
+                    setFeedback("Color changed to green...", 2000);
                     //setColor('#1aff80');
                     break;
                 case "setColorBlue":
                     setColor('blue');
+                    setFeedback("Color changed to blue...", 2000);
                     //setColor('#2ecfff');
                     break;
                 case "setColorAmber":
                     setColor('amber');
+                    setFeedback("Color changed to amber...", 2000);
                     //setColor('#ffb642');
                     break;
                 case "setColorWhite":
                     setColor('white');
+                    setFeedback("Color changed to white...", 2000);
                     //setColor('#c0ffff');
+                    break;
+                case "setDifficultyEasy":
+                    setDifficulty('easy');
+                    setFeedback("Difficulty changed to easy...", 2000);
+                    break;
+                case "setDifficultyMedium":
+                    setDifficulty('medium');
+                    setFeedback("Difficulty changed to medium...", 2000);
+                    break;
+                case "setDifficultyHard":
+                    setDifficulty('hard');
+                    setFeedback("Difficulty changed to hard...", 2000);
                     break;
             }
         });    
@@ -49,6 +65,19 @@ function submitActionsForm(action, e) {
 function setColor(color) {
     document.querySelector("body").className = color;
     localStorage.setItem("color", color);
-    
+    console.log('Color set to ' + color)
     //document.documentElement.style.setProperty('--color', color);
+}
+
+function setDifficulty(difficulty) {
+    localStorage.setItem("difficulty", difficulty);
+    console.log('Color difficulty to ' + difficulty)
+}
+
+function setFeedback(status, timeout) {
+    document.querySelector(".feedback .status").innerHTML = status;
+}
+
+function clearFeedback() {
+    document.querySelector(".feedback .status").innerHTML = "";
 }
