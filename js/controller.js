54f0fb6b2f63f354f35979fee920506610dcb7af
keaton22 <keaton22@gmail.com>
2017-07-12 01:11:53 -0400

    use reflexive references when refering to template types (menu or item)

--- js/controller.js
+++ js/controller.js
@@ -20,11 +20,11 @@ function ajax(file, method, callback, meta) {
 function dataHandler(response) {
     data = JSON.parse(response.responseText);
     console.groupCollapsed("got data from " + data.page + ".json");
-    
+
     for(i = 0; i < data.templates.length; i++) {
         var type = data.templates[i].type;
         var value = data.templates[i].value;
-        
+
         // run "ajax" function to get correct template, replaces switch statement
         ajax("html/_" + type +".html", "GET", templateHandler, {"type": type, "value": value});
         console.log("page has a " + type);
@@ -36,7 +36,7 @@ function dataHandler(response) {
 function templateHandler(response, meta) {
     template = response.responseText;
     console.groupCollapsed("got template _" + meta.type + ".html");
-    
+
     // run correct "write" function, replaces switch statement
     eval("write" + meta.type.charAt(0).toUpperCase() + meta.type.slice(1) + "(template, meta);");
     console.log("injected _" + meta.type + ".html into page");
@@ -54,14 +54,14 @@ function writeWelcome(template, meta) {
 // write menu
 function writeMenu(template, meta) {
     document.querySelector("#main").innerHTML += template;
-    
+
     for(i = 0; i < meta.value.length; i++) {
-        
+
         var li = document.createElement("li");
         var elem = document.createElement(meta.value[i].type);
         var text = document.createTextNode(meta.value[i].label);
         li.className = "item";
-        
+
         switch (meta.value[i].type) {
             case "a":
                 elem.setAttribute("href", meta.value[i].location);
@@ -74,16 +74,16 @@ function writeMenu(template, meta) {
                 elem.setAttribute("data-action", (meta.value[i].action || ""));
                 elem.setAttribute("data-result", (meta.value[i].result || ""));
                 break;
-        }        
-        
+        }
+
         elem.appendChild(text);
         li.appendChild(elem);
         document.querySelector("." + meta.type).appendChild(li);
-        console.log("injected \"" + meta.value[i].label + "\" into menu");
+        console.log("injected \"" + meta.value[i].label + "\" into " + meta.type);
     }
-    
-    document.querySelector(".menu .item").className += " selected";
-    document.querySelector(".menu .item").firstChild.focus();
+
+    document.querySelector("." + meta.type + " > *").className += " selected";
+    document.querySelector("." + meta.type + " > *").firstChild.focus();
 }
 
 function writeNote(template, meta) {
@@ -94,7 +94,7 @@ function writeNote(template, meta) {
 
 function templateLoaded() {
     templatesLoaded++;
-    
+
     if(templatesLoaded === requiredTemplates) {
         templatesLoaded = 0;
         console.groupEnd();
